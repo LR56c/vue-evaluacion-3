37c10975c2d09dec543e40919bc5da7b23d10960
Mauricio Herrera Ortega <ronalli56@gmail.com>
2024-12-22 21:51:18 -0300

    Initial Commit

---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import router from './router'
+
+// import {createBootstrap} from 'bootstrap-vue-next'
+// import 'bootstrap/dist/css/bootstrap.css'
+// import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
+// app.use(createBootstrap())
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+app.use(vuetify)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    <v-btn>
+      Button
+    </v-btn>
+  </main>
+</template>
